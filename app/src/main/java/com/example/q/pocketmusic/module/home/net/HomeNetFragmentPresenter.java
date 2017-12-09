f9b993b80022e7ab289927cc5f0c90b56b01ca6c
FortuenDream <812568684@qq.com>
2017-12-09 09:38:15 +0800

    4.2

--- app/src/main/java/com/example/q/pocketmusic/module/home/net/HomeNetFragmentPresenter.java
+++ app/src/main/java/com/example/q/pocketmusic/module/home/net/HomeNetFragmentPresenter.java
@@ -86,6 +86,7 @@ public class HomeNetFragmentPresenter extends BasePresenter<HomeNetFragmentPrese
     public boolean joinQQGroup() {
         String key = "CaPRvnhGGUsxrAr1vpZ1_2x-a-eib0N5";
         Intent intent = new Intent();
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.setData(Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D" + key));
         // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         try {
--- app/src/main/java/com/example/q/pocketmusic/module/song/bottom/SongMenuFragment.java
+++ app/src/main/java/com/example/q/pocketmusic/module/song/bottom/SongMenuFragment.java
@@ -91,11 +91,11 @@ public class SongMenuFragment extends BaseFragment<SongMenuPresenter.IView, Song
             case Constant.MENU_DOWNLOAD_COLLECTION_AGREE_SHARE://下载，收藏，点赞，分享
                 break;
             case Constant.MENU_DOWNLOAD_COLLECTION_SHARE://下载和收藏，分享
-                agreeIv.setVisibility(View.GONE);
+                agreeLl.setVisibility(View.GONE);
                 break;
             case Constant.MENU_DOWNLOAD_SHARE://下载，分享
-                agreeIv.setVisibility(View.GONE);
-                collectionIv.setVisibility(View.GONE);
+                agreeLl.setVisibility(View.GONE);
+                collectionLl.setVisibility(View.GONE);
                 break;
         }
     }
